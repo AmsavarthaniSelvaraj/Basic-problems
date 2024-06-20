6cc12201d66341b36e3f31a77ff5431e996a7c36
EniyaSelvaraj3 <s.amsavarthani@gmail.com>
2024-06-20 17:02:39 +0800

    Third

--- src/Divisible5.java
+++ src/Divisible5.java
@@ -1,9 +1,9 @@
 
 public class Divisible5 {
 	public static void main(String args[]) {
-//		int myInt = 35;
-//		boolean divisibleBy5 =myInt %5 ==0;
-//		boolean	divisibleBy7=myInt%7==0 ;
+    	int myInt = 35;
+		//boolean divisibleBy5 =myInt %5 ==0;
+		//boolean	divisibleBy7=myInt%7==0 ;
 		
 		
 		int i= 35;
@@ -15,26 +15,8 @@ public class Divisible5 {
 	{
 		System.out.println("not divisible by both 5 and 7");
 	}
-}
-	public static void sample(int a, int b, int c) {
-//		int myInt = 35;
-//		boolean divisibleBy5 =myInt %5 ==0;
-//		boolean	divisibleBy7=myInt%7==0 ;
-		
-		
-		int i= 35;
-		int j= 5;
-		int k = 7;
-		if(i%a==0 && i%b==0) {
-			System.out.println("divisible by both 5 and 7");
-	} else
-	{
-		System.out.println("not divisible by both 5 and 7");
-	}
-}
 
-}
-//git add .
-//git commit -m "second"
-// for check branch  - git branch
-//git push -u origin 
+	}}
+			
+		
+		
